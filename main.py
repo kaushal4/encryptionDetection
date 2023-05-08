f6cf9ef5d34ddb8eeec1a08f35b2fe88bd13553f
kaushal4 <damaniakaushal4@gmail.com>
2023-05-09 00:01:20 +0530

    updating high level main file

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
-import time
-import tracemalloc
-from psutil import cpu_count,Process
+from psutil import Process
 import os
-from encryptionDetection.watcher import DirectoryWatcher
+from metrices.metrices import Metrices
+from processEvaluation.evaluationController import EvaluationController
 
 
 def main():
-    p = Process(os.getpid())
-    tracemalloc.start()
-    directoryWatcher:DirectoryWatcher = DirectoryWatcher()
-    directoryWatcher.initiateWatch()
-    try:
-        while True:
-            current, peak = tracemalloc.get_traced_memory()
-            print(f"Current memory usage is {current / 10**3}KB")
-            print(f"Peak was {peak / 10**3}KB; Diff = {(peak - current) / 10**3}KB")
-            print(f"Cpu percentage is {p.cpu_percent()/cpu_count()}")
-            time.sleep(4)
+    ec = EvaluationController("6824",5)
+    ec.run()
+    ec.Terminate()
 
-    except KeyboardInterrupt:
-        directoryWatcher.terminateWatch()
-    tracemalloc.stop()
 
 if __name__ == "__main__":
     main()
